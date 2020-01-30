73f1bf98e4573bd5192579eabef4bc9808d3892c
Admin <zebogulrahimova@gmail.com>
2020-01-30 10:52:07 +0500

    initialized

---
+++ priority_queue.go
@@ -0,0 +1,78 @@
+package priority_queue
+
+type queue struct {
+	firstQueue *queueNode
+	lastQueue  *queueNode
+	size       int
+}
+
+type queueNode struct {
+	next  *queueNode
+	prev  *queueNode
+	value string
+}
+
+func (receiver *queue) lenQueue() int {
+	return receiver.size
+}
+
+func (receiver *queue) firstInQueue() interface{} {
+	if receiver.firstQueue == nil {
+		return nil
+	}
+	return receiver.firstQueue.value
+}
+
+func (receiver *queue) lastInQueue() interface{} {
+	if receiver.lastQueue == nil {
+		return nil
+	}
+	return receiver.lastQueue.value
+}
+
+func (receiver *queue) addLast(elementPtr string) {
+	if receiver.lenQueue() == 0 {
+		receiver.firstQueue = &queueNode{
+			next:  nil,
+			prev:  nil,
+			value: elementPtr,
+		}
+		receiver.size++
+		receiver.lastQueue = receiver.firstQueue
+		return
+	}
+	receiver.size++
+	current := receiver.firstQueue
+	for {
+		if current.next == nil {
+			current.next = &queueNode{
+				next:  nil,
+				prev:  current,
+				value: elementPtr,
+			}
+			receiver.lastQueue = current.next
+			return
+		}
+		current = current.next
+	}
+}
+
+func (receiver *queue) removeFirstFromQueue() {
+	if receiver.size == 0 {
+		return
+	}
+	if receiver.size == 1 {
+		receiver.firstQueue = nil
+		receiver.lastQueue = nil
+		receiver.size--
+		return
+	}
+	receiver.firstQueue = receiver.firstQueue.next
+	receiver.firstQueue.prev = nil
+	receiver.size--
+	return
+}
+
+func main() {
+
+}
---
+++ priority_queue_test.go
@@ -0,0 +1,81 @@
+package priority_queue
+
+import "testing"
+
+func Test_queue_with_empty_element(t *testing.T) {
+	queueList := queue{}
+	if queueList.lenQueue() != 0 {
+		t.Error("The queue length must be zero, got: ", queueList.lenQueue())
+	}
+	firstInQueue := queueList.firstInQueue()
+	if firstInQueue != nil {
+		t.Error("The first element must be nil from empty queue, got: ", firstInQueue)
+	}
+	lastInQueue := queueList.lastInQueue()
+	if lastInQueue != nil {
+		t.Error("The last element must be nil from empty queue, got: ", lastInQueue)
+	}
+	queueList.addLast("Jack")
+	if queueList.lenQueue() != 1 {
+		t.Error("The queue length must be equal 1, got: ", queueList.lenQueue())
+	}
+	queueList.removeFirstFromQueue()
+	if queueList.lenQueue() != 0 {
+		t.Error("The queue after remove element must be empty, got: ", queueList.lenQueue())
+	}
+}
+
+func Test_queue_with_one_element(t *testing.T) {
+	queueList := queue{}
+	queueList.addLast("Jack")
+	if queueList.lenQueue() != 1 {
+		t.Error("The queue length must be equal one, got: ", queueList.lenQueue())
+	}
+	firstInQueue := queueList.firstInQueue()
+	if firstInQueue != "Jack" {
+		t.Error("The first element in queue must be \"Jack\", got: ", firstInQueue)
+	}
+	lastInQueue := queueList.lastInQueue()
+	if lastInQueue != "Jack" {
+		t.Error("The last element in queue must be \"Jack\", got: ", lastInQueue)
+	}
+	queueList.addLast("Alex")
+	if queueList.lenQueue() != 2 {
+		t.Error("The queue length must be equal 2, got: ", queueList.lenQueue())
+	}
+	queueList.removeFirstFromQueue()
+	if queueList.lastInQueue() != queueList.firstInQueue() {
+		t.Error("The remove first element in queue \"Jack\",the first and the last must be \"Alex\" , got: ", "first in queue ", queueList.firstInQueue(), ", last in queue ", queueList.lastInQueue())
+	}
+}
+
+func Test_queue_with_multiple_element(t *testing.T) {
+	queueList := queue{}
+	queueList.addLast("Jack")
+	queueList.addLast("Alex")
+	queueList.addLast("Mike")
+	firstInQueue := queueList.firstInQueue()
+	if firstInQueue != "Jack" {
+		t.Error("The first in queue must be \"Jack\", got: ", firstInQueue)
+	}
+	lastInQueue := queueList.lastInQueue()
+	if lastInQueue != "Mike" {
+		t.Error("The last in queue must be \"Mike\", got: ", lastInQueue)
+	}
+	queueList.addLast("Anna")
+	if queueList.lastInQueue() != "Anna" {
+		t.Error("After add last in queue \"Anna\", the last must be, got: ", queueList.lastInQueue())
+	}
+	queueList.removeFirstFromQueue()
+	if queueList.firstInQueue() == "Jack" {
+		t.Error("After remove the first element in queue \"Jack\", the first in queue must be \"Alex\", got: ", queueList.firstInQueue())
+	}
+}
+
+func Test_remove_first_from_empty_queue(t *testing.T) {
+	queueList := queue{}
+	queueList.removeFirstFromQueue()
+	if queueList.lenQueue() != 0 {
+		t.Error("The queue length must be 0, got: ", queueList.lenQueue())
+	}
+}
